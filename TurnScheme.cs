6316b838fa60a93390ae152c730317d0884ce101
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-06 11:38:12 -0600

    Added several turn schemes

---
+++ TurnScheme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spite
+{
+    /// <summary>
+    /// Helps an Arena evaluate the order that turns are taken in.
+    /// </summary>
+    [Flags]
+    public enum TurnScheme : uint
+    {
+        /// <summary>
+        /// Turns are assigned to teams.
+        /// </summary>
+        Team = 0x0,
+        /// <summary>
+        /// Turns are assigned to entities one-by-one.
+        /// </summary>
+        Entity = 0x1,
+        /// <summary>
+        /// Sides decide what each entity on their team will do at the same time.
+        /// </summary>
+        VonNeumannTeam = 0x2,
+        /// <summary>
+        /// Sides decide what one entity will do at the same time.
+        /// </summary>
+        VonNeumannEntity = 0x3
+    }
+}

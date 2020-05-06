1561e20cdaa71d2574d4d94c9d0de7534a6e5c4c
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-05 18:58:24 -0600

    Added a basic entity interface.

---
+++ IEntity.cs
@@ -0,0 +1,40 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Represents an entity that the SpiteFramework can interact with.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// If this entity is untapped.
+        /// </summary>
+        bool IsTapped { get; }
+
+        /// <summary>
+        /// The team this entity belongs to.
+        /// </summary>
+        ITeam Team { get; }
+        
+        /// <summary>
+        /// Taps this entity.
+        /// </summary>
+        /// <returns>TRUE if the unit was successfully tapped.</returns>
+        bool Tap();
+
+        /// <summary>
+        /// Untaps this entity.
+        /// </summary>
+        /// <returns>TRUE if the unit was successfully untapped.</returns>
+        bool Untap();
+
+        /// <summary>
+        /// If this entity is still alive.
+        /// </summary>
+        bool IsAlive { get; }
+
+        /// <summary>
+        /// The id of the entity.
+        /// </summary>
+        EntityID ID { get; set; }
+    }
+}

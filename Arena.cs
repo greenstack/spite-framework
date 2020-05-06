60975d26f3b64882ad1d5231a558c401371359aa
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-06 11:37:41 -0600

    Added groundwork for the Arena class

---
+++ Arena.cs
@@ -0,0 +1,38 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Manages a group of sides in a battle.
+    /// </summary>
+    public class Arena
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly internal ITeam[] Sides;
+
+        /// <summary>
+        /// The number of sides managed by this arena.
+        /// </summary>
+        public int SideCount => Sides.Length;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numberOfSides"></param>
+        public Arena(uint numberOfSides)
+        {
+            Sides = new ITeam[numberOfSides];
+        }
+
+        /// <summary>
+        /// Gets the team with the given index.
+        /// </summary>
+        /// <param name="index">The index of the team.</param>
+        /// <returns>The team at the specified index</returns>
+        public ITeam GetTeam(uint index)
+        {
+            return Sides[index];
+        }
+
+    }
+}

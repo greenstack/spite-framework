31b25ade6ae0c807e430807efd40d7fd92fc2f56
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-06 11:38:58 -0600

    Began foundations of the ArenaBuilder

---
+++ ArenaBuilder.cs
@@ -0,0 +1,158 @@
+﻿using Spite.Properties;
+using System;
+
+namespace Spite
+{
+    /// <summary>
+    /// Represents a battle arena, with a number of sides and defining the flow
+    /// of battle.
+    /// </summary>
+    public class ArenaBuilder
+    {
+        private Arena builtArena;
+
+        private TeamBuilder currentTeam;
+
+        private int teamsAdded = 0;
+
+        private TurnScheme? turnScheme = null;
+
+        /// <summary>
+        /// Creates an instance of an Arena Builder.
+        /// </summary>
+        public ArenaBuilder()
+        {
+        }
+
+        /// <summary>
+        /// Sets the number of sides 
+        /// </summary>
+        /// <param name="sideCount">The number of sides fighting in this arena.</param>
+        /// <returns>The ArenaBuilder, for chaining.</returns>
+        public ArenaBuilder SetNumberOfSides(uint sideCount)
+        {
+            builtArena = new Arena(sideCount);
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the type of turn scheme that this arena will have.
+        /// </summary>
+        /// <param name="scheme">The scheme for determining the granularity of turns.</param>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder SetTurnScheme(TurnScheme scheme)
+        {
+            turnScheme = scheme;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a team of the specific type to the arena.
+        /// </summary>
+        /// <typeparam name="TTeam"></typeparam>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder AddTeam<TTeam>() where TTeam : ITeam, new()
+        {
+            AssertArenaWasMade();
+            if (teamsAdded == builtArena.SideCount)
+                throw new InvalidOperationException($"Cannot add another team to the arena - initialized with {builtArena.Sides.Length} sides");
+            currentTeam = new TeamBuilder().Start<TTeam>();
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder AddEntityToTeam(IEntity entity)
+        {
+            AssertArenaWasMade();
+            AssertTeamBuilder();
+            currentTeam.AddEntity(entity);
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the win condition for the current team.
+        /// </summary>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder SetWinConditionForTeam(Func<Arena, bool> winConFunc)
+        {
+            AssertArenaWasMade();
+            AssertTeamBuilder();
+            currentTeam.SetWinCondition(winConFunc);
+            return this;
+        }
+
+        /// <summary>
+        /// Finishes the team.
+        /// </summary>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder FinishTeam()
+        {
+            builtArena.Sides[teamsAdded++] = currentTeam.Finish();
+            return this;
+        }
+
+        /// <summary>
+        /// Finishes the team and provides it as a parameter.
+        /// </summary>
+        /// <param name="createdTeam">The team that get finished building.</param>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder FinishTeam(out ITeam createdTeam)
+        {
+            createdTeam = currentTeam.Finish();
+            builtArena.Sides[teamsAdded++] = createdTeam;
+            return this;
+        }
+
+        /// <summary>
+        /// Finishes the team and provides it as a parameter, cast to the specific type.
+        /// </summary>
+        /// <typeparam name="T">The team type.</typeparam>
+        /// <param name="createdTeam">The team that was created.</param>
+        /// <returns>The ArenaBuilder for chaining.</returns>
+        public ArenaBuilder FinishTeam<T>(out T createdTeam) where T : ITeam
+        {
+            createdTeam = currentTeam.Finish<T>();
+            builtArena.Sides[teamsAdded++] = createdTeam;
+            return this;
+        }
+
+        /// <summary>
+        /// Finishes building the Arena and returns the built arena.
+        /// </summary>
+        /// <returns>The built arena.</returns>
+        public Arena Finish()
+        {
+            AssertArenaWasMade();
+            return builtArena;
+        }
+
+        private void AssertArenaWasMade()
+        {
+#if DEBUG
+            if (builtArena == null)
+                throw new InvalidOperationException(Resources.ArenaNotStarted);
+#endif
+        }
+
+        private void AssertTeamBuilder()
+        {
+#if DEBUG
+            if (currentTeam == null)
+                throw new InvalidOperationException(Resources.NoTeamAddedArenaBuilder);
+#endif
+        }
+
+        private void AssertValidTurnScheme(TurnScheme expected, bool checkForNull)
+        {
+#if DEBUG
+            if (turnScheme != null && checkForNull && 
+                (turnScheme & TurnScheme.Entity) == expected)
+                throw new InvalidOperationException(Resources.InvalidTurnScheme);
+#endif
+        }
+    }
+}

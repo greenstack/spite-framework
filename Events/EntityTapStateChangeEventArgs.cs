4b7c64c37341e2a662024550ef5422516edf1688
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-06 21:33:03 -0600

    Added an Events namespace for future use

---
+++ Events/EntityTapStateChangeEventArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace Spite.Events
+{
+    /// <summary>
+    /// Contains information about the state change of the entity's tapped state.
+    /// </summary>
+    public class EntityTapStateChangeEventArgs : System.EventArgs
+    {
+        /// <summary>
+        /// If the entity became tapped during this event.
+        /// </summary>
+        public bool BecameTapped { get; }
+
+        /// <summary>
+        /// Creates a default EntityTapStateChangeEventArgs object.
+        /// </summary>
+        /// <param name="becameTapped">If the entity</param>
+        public EntityTapStateChangeEventArgs(bool becameTapped)
+        {
+            BecameTapped = becameTapped;
+        }
+    }
+}

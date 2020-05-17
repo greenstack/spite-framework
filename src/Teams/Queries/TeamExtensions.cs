ab08468844655d3f444fb1540b8d929f7a5df297
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-16 22:19:16 -0600

    Added extension methods to query teams

---
+++ src/Teams/Queries/TeamExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Spite.Teams.Queries
+{
+    /// <summary>
+    /// Provides various methods to query teams with.
+    /// </summary>
+    public static class TeamQueryExtensions
+    {
+        /// <summary>
+        /// Counts the number of entities still alive on the team.
+        /// </summary>
+        /// <param name="team">The team to query.</param>
+        /// <returns>The number of living entities.</returns>
+        public static int CountLivingEntities(this ITeam team)
+        {
+            if (team == null) throw new ArgumentNullException(nameof(team));
+            return team.Entities.Count(e => e.IsAlive);
+        }
+
+        /// <summary>
+        /// Determines if all entities are alive on the team.
+        /// </summary>
+        /// <param name="team">The team to query.</param>
+        /// <returns>True if all entities on this team are alive.</returns>
+        public static bool AreAllEntitiesAlive(this ITeam team)
+        {
+            if (team == null) throw new ArgumentNullException(nameof(team));
+            return team.CountLivingEntities() == team.ManagedEntityCount;
+        }
+    }
+}

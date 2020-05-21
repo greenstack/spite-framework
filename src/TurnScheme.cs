95b4cfbee1423d99e983d1ce43ae73f03df6000b
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-20 20:38:40 -0600

    TurnScheme provides a few examples

--- src/TurnScheme.cs
+++ src/TurnScheme.cs
@@ -7,18 +7,27 @@
     {
         /// <summary>
         /// Turns are assigned to teams.
+        /// 
+        /// Examples: Fire Emblem, Battleship
         /// </summary>
         Team = 0x0,
         /// <summary>
         /// Turns are assigned to entities one-by-one.
+        /// 
+        /// Examples: Octopath Traveler, any game where entities act, sorted by
+        /// something like an entity's speed.
         /// </summary>
         Entity = 0x1,
         /// <summary>
         /// Sides decide what each entity on their team will do at the same time.
+        /// 
+        /// Examples: Pokemon, Rock Paper Scissors/Roshambo
         /// </summary>
         VonNeumannTeam = 0x2,
         /// <summary>
         /// Sides decide what one entity will do at the same time.
+        /// 
+        /// Examples: I dunno
         /// </summary>
         VonNeumannEntity = 0x3
     }

0e3b074374425deee820d40d1ac0ae579ee4911d
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-20 20:39:00 -0600

    Added IArena

---
+++ src/IArena.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Spite
+{
+    /// <summary>
+    /// Manages a group of sides in a battle.
+    /// </summary>
+    public interface IArena
+    {
+        /// <summary>
+        /// All the teams/sides that are fighting in this arena.
+        /// </summary>
+        ICollection<ITeam> Sides { get; }
+
+        /// <summary>
+        /// The number of sides managed by this arena.
+        /// </summary>
+        int SideCount { get; }
+
+        /// <summary>
+        /// The name of this arena.
+        /// </summary>
+        string ArenaName { get; }
+    }
+}

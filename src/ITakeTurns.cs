00d5f9be67f5e22a44b1ec873d003f8412df4ff7
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-20 20:24:12 -0600

    Created an interface for taking turns.

---
+++ src/ITakeTurns.cs
@@ -0,0 +1,20 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Represents an item that can have turns assigned to it.
+    /// </summary>
+    public interface ITakeTurns
+    {
+        /// <summary>
+        /// Informs this item that its turn is beginning.
+        /// </summary>
+        /// <param name="context">The arena this item is participating in.</param>
+        void BeginTurn(IArena context);
+
+        /// <summary>
+        /// Informs this object that its turn is ending.
+        /// </summary>
+        /// <param name="context">The arena this item is participating in.</param>
+        void EndTurn(IArena context); 
+    }
+}

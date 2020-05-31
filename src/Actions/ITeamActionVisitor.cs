9bbec2e30e9fec13b0beda69a817012a6d4d29d7
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-30 20:48:43 -0600

    Added a visitor class that can visit actions.

---
+++ src/Actions/ITeamActionVisitor.cs
@@ -0,0 +1,15 @@
+﻿namespace Spite.Actions
+{
+    /// <summary>
+    /// Provides an interface for IActionVisitor objects.
+    /// </summary>
+    public interface ITeamActionVisitor
+    {
+        /// <summary>
+        /// Visits a teamAction.
+        /// </summary>
+        /// <typeparam name="TTeam">The type of team the action is impacted.</typeparam>
+        /// <param name="teamAction">The action being visited.</param>
+        void Visit<TTeam>(ITeamAction<TTeam> teamAction) where TTeam : ITeam;
+    }
+}

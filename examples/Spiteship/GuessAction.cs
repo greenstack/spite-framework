3c61052d6fd597d624115be75d9d35b435e3e049
Joseph Newman <paradoxbuilder@gmail.com>
2020-05-30 20:13:12 -0600

    Added a GuessAction for the program

---
+++ examples/Spiteship/GuessAction.cs
@@ -0,0 +1,27 @@
+﻿using Spite;
+using Spite.Actions;
+
+namespace SpiteBattleship
+{
+    class GuessAction : ITeamAction<BattleshipTeam>
+    {
+        public BattleshipTeam Target { get; }
+
+        public IArena Context { get; }
+
+        int x, y;
+
+        public GuessAction(BattleshipTeam target, IArena context, int x, int y)
+        {
+            Target = target;
+            Context = context;
+            this.x = x;
+            this.y = y;
+        }
+
+        public IActionResult Execute()
+        {
+            return new GuessActionResult(Target.ReceiveGuess(x, y));
+        }
+    }
+}
---
+++ examples/Spiteship/GuessActionResult.cs
@@ -0,0 +1,12 @@
+﻿namespace SpiteBattleship
+{
+    class GuessActionResult : Spite.Actions.IActionResult
+    {
+        public bool Success { get; }
+
+        public GuessActionResult(bool success)
+        {
+            Success = success;
+        }
+    }
+}

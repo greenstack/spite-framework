2f080828453b8c192fe3231b006a55a46d2041a1
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-05 23:07:18 -0600

    Added a generic tappable interface

---
+++ src/ITappable.cs
@@ -0,0 +1,30 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Represents an object that can be tapped. Used in the Arena's turn management system.
+    /// </summary>
+    public interface ITappable
+    {
+        /// <summary>
+        /// Whether or not this object can act.
+        /// </summary>
+        bool Tapped { get; }
+
+        /// <summary>
+        /// Fired when the object is untapped.
+        /// </summary>
+        event Untap OnUntap;
+
+        /// <summary>
+        /// Fired when the object is tapped.
+        /// </summary>
+        event Tap OnTapped;
+    }
+
+    /// <summary>
+    /// Represents when the Tappable is untapped.
+    /// </summary>
+    public delegate void Untap();
+
+    public delegate void Tap();
+}

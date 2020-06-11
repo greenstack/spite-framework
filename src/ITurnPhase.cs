b5672cb41ea92cb6774d0db315edc13effbecc40
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-10 21:39:46 -0600

    Added an interface to represent the phases of a turn

---
+++ src/ITurnPhase.cs
@@ -0,0 +1,28 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Represents a phase in a turn. These should be managed by the turn
+    /// manager, and act as the manager's state.
+    /// </summary>
+    public interface ITurnPhase
+    {
+        /// <summary>
+        /// Changes the phase. Should invoke the <see cref="OnPhaseChanged"/> event.
+        /// </summary>
+        /// <param name="manager">The turn manager this phase belongs to.</param>
+        void ChangePhase(ITurnManager manager);
+
+        /// <summary>
+        /// Triggered when a phase changes.
+        /// </summary>
+        event ChangePhase OnPhaseChanged;
+    }
+
+    /// <summary>
+    /// A delegate to be called when a turn's phase changes.
+    /// </summary>
+    /// <param name="fromPhase">The phase being transitioned from.</param>
+    /// <param name="toPhase">The phase being transitioned to.</param>
+    /// <param name="turnManager">The turn manager.</param>
+    public delegate void ChangePhase(ITurnPhase fromPhase, ITurnPhase toPhase, ITurnManager turnManager);
+}

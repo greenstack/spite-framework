9b62b4bb184430323435312a1dab530ed07af63f
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-13 21:04:25 -0600

    Added a simple class that represents the result of a failed action

---
+++ src/Actions/ActionFailedResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Spite.Actions
+{
+    /// <summary>
+    /// A simple implementation of IActionResult for a failed action with no further context.
+    /// </summary>
+    public class ActionFailedResult : IActionResult
+    {
+        /// <summary>
+        /// Returns false.
+        /// </summary>
+        public bool Success => false;
+    }
+}

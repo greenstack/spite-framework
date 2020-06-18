cd5e5a039ce96d29e423fdd67a25aed51ce5847f
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-17 21:52:10 -0600

    Created a base class for BattleshipTurnPhases.

---
+++ examples/Spiteship/BattleshipTurnPhase.cs
@@ -0,0 +1,17 @@
+﻿using Spite;
+using Spite.Actions;
+
+namespace SpiteBattleship
+{
+    abstract class BattleshipTurnPhase : ITurnPhase
+    {
+        public event ChangePhase OnPhaseChanged;
+
+        public void ChangePhase(ITurnManager manager)
+        {
+            OnPhaseChanged?.Invoke(this, null, manager);
+        }
+
+        public abstract BattleshipAction GetAction(IArena context, BattleshipTurnManager turnManager);
+    }
+}

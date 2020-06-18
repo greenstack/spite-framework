7da157a0d0a1a0b5ec1abf915134ceb125a2b6a9
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-17 21:52:38 -0600

    Added a BattleshipTurnManager class to manage turns in Battleship

---
+++ examples/Spiteship/BattleshipTurnManager.cs
@@ -0,0 +1,47 @@
+﻿using Spite;
+using Spite.Actions;
+using Spite.Queries;
+
+namespace SpiteBattleship
+{
+    class BattleshipTurnManager : ITurnManager
+    {
+        private readonly PlayerBattleshipController player;
+
+        public BattleshipTurnManager(PlayerBattleshipController player)
+        {
+            this.player = player;
+            currentPhase = new PlayerPhase();
+        }
+
+        public ITurnController CurrentController => player;
+
+        private BattleshipTurnPhase currentPhase;
+        public ITurnPhase CurrentPhase
+        {
+            get => currentPhase;
+            set
+            {
+                OnPhaseChanged?.Invoke(currentPhase, value, this);
+                currentPhase = (BattleshipTurnPhase)value;
+            }
+        }
+
+        public event ChangePhase OnPhaseChanged;
+
+        public bool CanControllerAct(ITurnController actor, IAction action)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DoTurn(IArena arena)
+        {
+            var action = currentPhase.GetAction(arena, this);
+            action.Execute();
+            arena.UpdateTeamStandings();
+            return arena.AnyTeamHasStanding(TeamStanding.Eliminated);
+        }
+
+    }
+}
+ 

24453971289e9200576050674cb06977d0724923
Joseph Newman <paradoxbuilder@gmail.com>
2020-06-17 21:53:27 -0600

    Added a PlayerPhase class

---
+++ examples/Spiteship/PlayerPhase.cs
@@ -0,0 +1,31 @@
+﻿using Spite;
+using System.Linq;
+
+namespace SpiteBattleship
+{
+    class PlayerPhase : BattleshipTurnPhase
+    {
+        public static PlayerBattleshipController player;
+
+        public override BattleshipAction GetAction(IArena arena, BattleshipTurnManager turnManager)
+        {
+            int x = 0, y = 0;
+            bool quit = player.askPlayerForInput(ref x, ref y);
+            if (quit)
+            {
+                return new QuitCommand(arena, player.Team);
+            }
+            else
+            {
+                var opponent = arena.GetTeamsOpposing(player.Team).First();
+                return new GuessAction(
+                    player.Team as BattleshipTeam,
+                    opponent as BattleshipTeam,
+                    arena,
+                    x,
+                    y
+                );
+            }
+        }
+    }
+}

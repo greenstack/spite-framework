465181e57c5d8031a0e0fb3c3ddfa6b49ded42f2
Joseph Newman <paradoxbuilder@gmail.com>
2020-07-03 12:21:52 -0600

    Issue #2: TeamBuilder captures the type of team it's producing

--- src/TeamBuilder.cs
+++ src/TeamBuilder.cs
@@ -5,16 +5,16 @@ namespace Spite
     /// <summary>
     /// Provides methods useful in building a team.
     /// </summary>
-    public class TeamBuilder
+    public class TeamBuilder<TTeam> where TTeam : ITeam, new()
     {
-        private ITeam builtTeam;
+        private TTeam builtTeam;
 
         /// <summary>
         /// Starts building the team with a fresh instance of the specified team type.
         /// </summary>
         /// <typeparam name="TTeam">The type of team to create.</typeparam>
         /// <returns>The team builder for chaining.</returns>
-        public TeamBuilder Start<TTeam>() where TTeam : ITeam, new()
+        public TeamBuilder<TTeam> Start()
         {
             builtTeam = new TTeam();
             return this;
@@ -25,7 +25,7 @@ namespace Spite
         /// </summary>
         /// <param name="teamSize">The initial or max size of the team.</param>
         /// <returns>The team builder for chaining.</returns>
-        public TeamBuilder SetTeamSize(uint teamSize)
+        public TeamBuilder<TTeam> SetTeamSize(uint teamSize)
         {
             builtTeam.InitializeEntityCount(teamSize);
             return this;
@@ -36,7 +36,7 @@ namespace Spite
         /// </summary>
         /// <param name="entity">The entity to add to the team.</param>
         /// <returns>The TeamBuilder for chaining.</returns>
-        public TeamBuilder AddEntity(IEntity entity)
+        public TeamBuilder<TTeam> AddEntity(IEntity entity)
         {
             builtTeam.AddEntity(entity);
             return this;
@@ -47,7 +47,7 @@ namespace Spite
         /// </summary>
         /// <param name="winConFunc">The function that will be used to determine the win conditions.</param>
         /// <returns>The TeamBuilder for chaining.</returns>
-        public TeamBuilder SetTeamStandingDeterminer(Func<Arena, TeamStanding> winConFunc)
+        public TeamBuilder<TTeam> SetTeamStandingDeterminer(Func<Arena, TeamStanding> winConFunc)
         {
             throw new NotImplementedException();
         }
@@ -55,27 +55,9 @@ namespace Spite
         /// Finishes building the team.
         /// </summary>
         /// <returns>The built team.</returns>
-        public ITeam Finish()
+        public TTeam Finish()
         {
             return builtTeam;
-        }
-
-        /// <summary>
-        /// Finsihes building the team and casts it to the specific type.
-        /// </summary>
-        /// <typeparam name="TTeam">The team type to cast to. Should be the same type as used in TTeam.</typeparam>
-        /// <returns>The team of the specified type.</returns>
-        public TTeam Finish<TTeam>() where TTeam : ITeam
-        {
-            if (builtTeam == null)
-            {
-                throw new InvalidOperationException(Properties.Resources.START_NOT_CALLED_ERROR_MESSAGE);
-            }
-            if (!(builtTeam is TTeam))
-            {
-                throw new InvalidCastException($"Can't cast the built team to {typeof(TTeam)}. Check the typeparam of Start and Finish.");
-            }
-            return (TTeam)builtTeam;
         }
     }
 }

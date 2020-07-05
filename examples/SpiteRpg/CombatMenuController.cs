99f8458f13e6130b5b9e8014b0c06254add0389c
Joseph Newman <paradoxbuilder@gmail.com>
2020-07-04 23:42:13 -0600

    Adding CombatMenu to RPG

---
+++ examples/SpiteRpg/CombatMenuController.cs
@@ -0,0 +1,58 @@
+﻿using Spite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiteRpg
+{
+    class CombatMenuController
+    {
+
+        private readonly CombatMenu menu;
+        private readonly Arena<RpgTeam, RpgEntity> arena;
+        private RpgTeam playerTeam;
+
+        // TODO: Have this managed by the arena or the turn manager
+        private int currentPhase = 0;
+
+        private int selectedIndex = 0;
+
+        public CombatMenuController(CombatMenu menu, Arena<RpgTeam, RpgEntity> arena)
+        {
+            this.menu = menu;
+            this.arena = arena;
+            playerTeam = arena.Teams[0];
+        }
+
+        public void ReadInput()
+        {
+            var input = Console.ReadKey(true);
+            if (currentPhase != 2 && input.Key == ConsoleKey.Enter)
+            {
+                currentPhase++;
+            }
+            else if (input.Key == ConsoleKey.Enter)
+            {
+                currentPhase = 0;
+            }
+            else if (currentPhase != 0 && input.Key == ConsoleKey.Backspace)
+            {
+                currentPhase--;
+            }
+            else if (input.Key == ConsoleKey.LeftArrow)
+            {
+                selectedIndex += 2;
+                selectedIndex %= 3;
+                menu.actors[currentPhase] = (RpgEntity)playerTeam.Entities.ElementAt(selectedIndex);
+            }
+            else if (input.Key == ConsoleKey.RightArrow)
+            {
+                selectedIndex++;
+                selectedIndex %= 3;
+                menu.actors[currentPhase] = (RpgEntity)playerTeam.Entities.ElementAt(selectedIndex);
+            }
+        }
+    }
+}

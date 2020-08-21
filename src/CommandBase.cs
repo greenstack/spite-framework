a4a693a44ef762c1f4029c4efdcf8d3e476a0570
Joseph Newman <paradoxbuilder@gmail.com>
2020-08-21 12:48:21 -0600

    Added a base command class

---
+++ src/CommandBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Spite
+{
+    /// <summary>
+    /// Provides a basic structure and implementation for commands.
+    /// </summary>
+    public abstract class CommandBase : ICommand
+    {
+        public IActor Owner { get; }
+        
+        public IArena Context { get; }
+
+        public abstract bool ShouldUpdateTeamStandings { get; }
+
+
+        public CommandBase(IActor owner, IArena context)
+        {
+            Owner = owner;
+            Context = context;
+        }
+
+        public abstract bool Execute();
+    }
+}

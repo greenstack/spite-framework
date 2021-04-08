c61d8ddf82dae3e681e9411d81c89a587fc55778
Joseph Newman <paradoxbuilder@gmail.com>
2021-04-08 16:47:23 -0600

    Set MockTurnManager phase in constructor

--- tests/Mocks/Turns/MockTurnManager.cs
+++ tests/Mocks/Turns/MockTurnManager.cs
@@ -1,11 +1,12 @@
 using Spite.Turns;
 
-namespace Spite.UnitTests.Mocks
+namespace Spite.UnitTests.Mocks.Turns
 {
     class MockTurnManager : TurnManagerBase
     {
         public MockTurnManager(bool executeFollowUpsIfActionFailed) : base(executeFollowUpsIfActionFailed)
         {
+            CurrentPhase = new AlwaysExecutableTurnPhase();
         }
     }
 }

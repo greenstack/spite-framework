74972a786ec4caf57b9ef7dd2722fa380bae3b49
Joseph Newman <paradoxbuilder@gmail.com>
2021-04-14 16:03:06 -0600

    Remvoed ITurnManager.CanAct

--- src/Turns/ITurnManager.cs
+++ src/Turns/ITurnManager.cs
@@ -18,13 +18,6 @@ namespace Spite.Turns
         event ChangePhase OnPhaseChanged;
 
         /// <summary>
-        /// Determines if the provided actor is allowed to submit a command, has priority, or otherwise.
-        /// </summary>
-        /// <param name="actor">The actor to query for.</param>
-        /// <returns>True if the given actor is allowed to act.</returns>
-        bool CanAct(IActor actor);
-
-        /// <summary>
         /// Tries to execute the command.
         /// </summary>
         /// <returns>All of the reactions that arise as a result of the command or null if the command is invalid or cannot be executed.</returns>

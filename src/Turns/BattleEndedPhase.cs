9b97d9a200a717a793ca5bb920b388c1c45d5320
Joseph Newman <paradoxbuilder@gmail.com>
2021-04-14 21:28:01 -0600

    Created a specific phase for end of battle.

---
+++ src/Turns/BattleEndedPhase.cs
@@ -0,0 +1,24 @@
+﻿using Spite.Interaction;
+
+namespace Spite.Turns
+{
+    /// <summary>
+    /// Represents a battle that has ended. This is the canonical "end of
+    /// battle" phase, but it can be extended to suit your needs.
+    /// </summary>
+    class BattleEndedPhase : ITurnPhase
+    {
+        public event ChangePhase OnPhaseChanged;
+
+        public void ChangePhase(ITurnManager manager)
+        {
+            // Do nothing. The battle is over; there is nothing to do.
+        }
+
+        public bool IsCommandExecutableThisPhase(CommandBase command)
+        {
+            // No commands can be executed once the battle is over.
+            return false;
+        }
+    }
+}

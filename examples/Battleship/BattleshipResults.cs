0d1a7e2606586c81ab1ef399fd73f197b62d2e37
Joseph Newman <paradoxbuilder@gmail.com>
2021-04-15 17:11:24 -0600

    Added reactions for battleship.

---
+++ examples/Battleship/BattleshipResults.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spite;
+using Spite.Interaction;
+
+namespace Battleship
+{
+	class ForfeitReaction : IReaction
+	{
+		private readonly ITeam forfeitedTeam;
+
+		public ForfeitReaction(ITeam forfeitedTeam)
+		{
+			this.forfeitedTeam = forfeitedTeam;
+		}
+
+		public ISpiteAction FollowUpAction => null;
+
+		public bool ActionSuccessful => true;
+
+		public object GetReactionData()
+		{
+			return null;
+		}
+
+		public TReactionData GetReactionData<TReactionData>()
+		{
+			return default;
+		}
+
+		public override string ToString()
+		{
+			return $"{forfeitedTeam} forfeit!";
+		}
+	}
+}

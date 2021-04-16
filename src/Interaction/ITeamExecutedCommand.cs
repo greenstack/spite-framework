9bafca104c537315465b13db9933acb760f5b350
Joseph Newman <paradoxbuilder@gmail.com>
2021-04-15 22:18:48 -0600

    Added the interfaces for the leveled commands

---
+++ src/Interaction/ITeamExecutedCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Spite.Interaction
+{
+    /// <summary>
+    /// Represents a command executed by a team.
+    /// </summary>
+    public interface ITeamExecutedCommand : ISpiteCommand
+    {
+        /// <summary>
+        /// The team executing this command.
+        /// </summary>
+        ITeam Executor { get; }
+    }
+}
---
+++ src/Interaction/ITeammateExecutedCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Spite.Interaction
+{
+	/// <summary>
+	/// Provides an interface for commands that are executed by teammates.
+	/// </summary>
+	public interface ITeammateExecutedCommand : ISpiteCommand
+	{
+		/// <summary>
+		/// The teammate performing the action.
+		/// </summary>
+		ITeammate Executor { get; }
+	}
+}

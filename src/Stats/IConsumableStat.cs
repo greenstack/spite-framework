ad67e49bf0740b0ae7612d5dce022b37072b881a
Joseph Newman <paradoxbuilder@gmail.com>
2021-06-08 13:37:29 -0600

    Issue #18: Improved the interfaces that IConsumableStat uses.

--- src/Stats/IConsumableStat.cs
+++ src/Stats/IConsumableStat.cs
@@ -4,18 +4,20 @@ namespace Spite.Stats
 	/// Provides an interface for stats that are consumed.
 	/// </summary>
 	/// <typeparam name="T">The underlying numeric type for the stat.</typeparam>
-	public interface IConsumableStat<T> : IMaxableStat<T>
+	public interface IConsumableStat<T> : IMaxableStat<T>, IMinableStat<T> where T :
+		System.IComparable<T>,
+		System.IEquatable<T>
 	{
 		/// <summary>
 		/// Removes the specified amount from the stat's current value.
 		/// </summary>
 		/// <param name="amount">The amount of the stat to drain.</param>
-		void Drain(T amount);
+		void Add(T amount);
 
 		/// <summary>
 		/// Adds the specified amount to the stat's value.
 		/// </summary>
 		/// <param name="amount">The amount of the stat to replenish.</param>
-		void Replenish(T amount);
+		void Remove(T amount);
 	}
 }

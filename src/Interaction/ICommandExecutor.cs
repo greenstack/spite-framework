49276cb5f4542fd4e6843edfad1a43115297bfa5
Joseph Newman <paradoxbuilder@gmail.com>
2021-06-09 16:03:02 -0600

    Issue #20: Added a command executor interface for anything that can execute a command.

---
+++ src/Interaction/ICommandExecutor.cs
@@ -0,0 +1,13 @@
+﻿namespace Spite.Interaction
+{
+	/// <summary>
+	/// Provides an interface for objects that can execute commands.
+	/// </summary>
+	public interface ICommandExecutor
+	{
+		/// <summary>
+		/// The team responsible for this executor.
+		/// </summary>
+		ITeam ResponsibleTeam { get; }
+	}
+}

986a32a0d03a34c7b70716ec8f4eb461134906ad
Joseph Newman <paradoxbuilder@gmail.com>
2021-06-10 13:19:16 -0600

    Issue #24: Added code that lets users tap all units on a team at once.

---
+++ src/Interaction/TapAllReaction.cs
@@ -0,0 +1,26 @@
+﻿namespace Spite.Interaction
+{
+	/// <summary>
+	/// The reaction for a commmand that tapped all units.
+	/// </summary>
+	public class TapAllReaction : IReaction
+	{
+		/// <inheritdoc/>
+		public ISpiteAction FollowUpAction => null;
+
+		/// <inheritdoc/>
+		public bool ActionSuccessful => true;
+
+		/// <inheritdoc/>
+		public object GetReactionData()
+		{
+			return this;
+		}
+
+		/// <inheritdoc/>
+		public TReactionData GetReactionData<TReactionData>()
+		{
+			return default;
+		}
+	}
+}

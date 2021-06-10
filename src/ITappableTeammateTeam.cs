b324dccfcdc305a90f40a27eac3b4712c6802cf4
Joseph Newman <paradoxbuilder@gmail.com>
2021-06-09 19:47:37 -0600

    Issue #20: Interface for teams composed of ITappableTeammates.

---
+++ src/ITappableTeammateTeam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spite
+{
+	/// <summary>
+	/// A team of entities that can be tapped.
+	/// </summary>
+	public interface ITappableTeammateTeam : ITeam<ITappableTeammate> { }
+
+	/// <summary>
+	/// A generic team of entities that can be tapped.
+	/// </summary>
+	/// <typeparam name="T">The type of entity to contain.</typeparam>
+	public interface ITappableTeammateTeam<T> : ITappableTeammateTeam
+		where T : ITappableTeammate
+	{
+	}
+}

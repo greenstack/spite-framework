6947203232503fbd35728e9d5388c60cccd06f13
Joseph Newman <paradoxbuilder@gmail.com>
2021-06-09 21:12:33 -0600

    Issue #20: Made MockTeam okay for more unit tests

--- tests/MockTeam.cs
+++ tests/MockTeam.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
+using Spite.Interaction;
+using Spite.UnitTests.Mocks;
+
 namespace Spite.UnitTests
 {
     /// <summary>
     /// A mock team object.
     /// </summary>
-    class MockTeam : ITeam
+    class MockTeam : ITappableTeammateTeam<MockTappableTeammate>, ICommandExecutor
     {
         public TeamStanding CurrentStanding => TeamStanding.Inactive;
 
-        public TeamStanding DetermineStanding(IArena context)
+		private readonly List<ITappableTeammate> teammates = new List<ITappableTeammate>();
+		public ICollection<ITappableTeammate> Members => teammates;
+
+		public int ManagedEntityCount => teammates.Count;
+
+		public ITeam ResponsibleTeam => this;
+
+		public void AddEntity(ITappableTeammate entity)
+		{
+			teammates.Add(entity);
+		}
+
+		public TeamStanding DetermineStanding(IArena context)
         {
             return CurrentStanding;
         }
@@ -16,5 +32,10 @@ namespace Spite.UnitTests
         {
             throw new System.NotImplementedException();
         }
-    }
+
+		public void InitializeEntityCount(uint entityCount)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }

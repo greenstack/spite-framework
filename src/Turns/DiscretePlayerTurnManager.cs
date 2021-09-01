dfd7b43a9c4b5fd96dba52a82a616f282fb29fcf
Joseph Newman <paradoxbuilder@gmail.com>
2021-08-31 20:02:49 -0600

    Issue #39: Added an IPlayer interface for internal use (specifically, the ArenaBuilder).

--- src/Turns/DiscretePlayerTurnManager.cs
+++ src/Turns/DiscretePlayerTurnManager.cs
@@ -4,6 +4,16 @@ using System.Collections.Generic;
 namespace Spite.Turns
 {
 	/// <summary>
+	/// An internal-use only interface for player objects.
+	/// 
+	/// I have this marked as internal because I don't want devs to believe that
+	/// a player needs to implement this interface, since even though the player
+	/// issues the commands, the player is rarely the executor of those commands
+	/// (like in Pokemon, Fire Emblem, Final Fantasy, Earthbound, etc.)
+	/// </summary>
+	internal interface IPlayer : ITeam, ICommandExecutor { }
+
+	/// <summary>
 	/// Represents a turn manager that allows players to make decisions for as long
 	/// as that player has priority.
 	/// 
@@ -15,8 +25,14 @@ namespace Spite.Turns
 	{
 		/// <inheritdoc/>
 		public DiscretePlayerTurnManager(IList<TPlayer> players, bool executeFollowUpsIfActionFailed = true) :
-			base(players, null, executeFollowUpsIfActionFailed)
+			base(players, new PlayerPhase<TPlayer>(GetFirstTeam(players)), executeFollowUpsIfActionFailed)
 		{
+		}
+
+		/// <inheritdoc/>
+		protected override ITurnPhase GetNextPhase()
+		{
+			return new PlayerPhase<TPlayer>(AdvanceToNextTeam());
 		}
 	}
 }

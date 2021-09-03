48c8c90de1daab4fe29371a43b6dd8a96f40bdda
Joseph Newman <paradoxbuilder@gmail.com>
2021-09-03 11:00:16 -0600

    Added MockPlayer class for future unit tests.

---
+++ tests/Mocks/MockPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Spite.Turns;
+
+namespace Spite.UnitTests.Mocks
+{
+	public class MockPlayer : ITeamExecutor<MockTeammate>
+	{
+		private readonly List<MockTeammate> teammates = new List<MockTeammate>();
+		public ICollection<MockTeammate> Members => teammates;
+
+		public TeamStanding CurrentStanding {get; set;} = TeamStanding.Inactive;
+
+		public int ManagedEntityCount => Members.Count;
+
+		public ITeam ResponsibleTeam => this;
+
+		public void AddEntity(MockTeammate entity)
+		{
+			teammates.Add(entity);
+		}
+
+		public TeamStanding DetermineStanding(IArena context)
+		{
+			return CurrentStanding;
+		}
+
+		public void ForceStanding(TeamStanding standing)
+		{
+			CurrentStanding = standing;
+		}
+
+		public void InitializeEntityCount(uint entityCount)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
